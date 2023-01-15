e7090a1b60352ea669e07a9838aa845fcc3ce6e3
Bruno Vieira <brunorick11@gmail.com>
2023-01-15 10:43:46 -0300

    function cadastro controller

--- backend/controllers/ClienteController.js
+++ backend/controllers/ClienteController.js
@@ -1,7 +1,37 @@
 const Cliente = require("../models/Cliente");
 
-module.exports = class ClienteController{
-    static async register(req, res){
-        res.json('ola')
+module.exports = class ClienteController {
+  // Cadastro de clientes
+  static async register(req, res) {
+    const { nome, peso_produto, endereco } = req.body;
+    // Validações
+    if (!nome) {
+      res.status(400).json({ message: "O nome é obrigatório" });
+      return;
     }
-}
+    if (!peso_produto) {
+      res.status(400).json({ message: "O peso do produto é obrigatório" });
+      return;
+    }
+    if (!endereco) {
+      res.status(400).json({ message: "O endereco é obrigatório" });
+      return;
+    }
+    // Salvando cliente no banco
+    const cliente = new Cliente({
+      nome,
+      peso_produto,
+      endereco: {},
+    });
+    try {
+      const newCliente = await cliente.save();
+      res.status(201).json({
+        message: "Cliente cadastrado com sucesso!",
+        newCliente,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+  //
+};
--- backend/routes/ClienteRoutes.js
+++ backend/routes/ClienteRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-
-const ClienteController = require('../controllers/ClienteController')
+const ClienteController = require("../controllers/ClienteController");
 
 router.post("/register", ClienteController.register);
+
 
 module.exports = router;
